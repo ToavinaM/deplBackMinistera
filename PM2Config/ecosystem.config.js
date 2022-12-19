4e86f5030b699ec08c817331da1ad6602e823790
ETU936 <80760102+ToavinaM@users.noreply.github.com>
2022-12-19 10:27:00 +0300

    pm2 config
    run ex

---
+++ PM2Config/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: "API-Operationnel",
+    script: "../deplBackMinistera/server.js"
+  },
+  {
+    name: "FRONT-Operationnel",
+    script: "../MinisteraFront/index.js",
+  }
+  ]
+}
